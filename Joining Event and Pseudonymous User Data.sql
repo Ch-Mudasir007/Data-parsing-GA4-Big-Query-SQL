be7ea0dd3d79a06d5749975ef8dfebf4bee70e5f
Muhammad Mudasir Shafiq <155460139+Ch-Mudasir007@users.noreply.github.com>
2024-11-06 17:20:47 +0100

    Update Joining Event and Pseudonymous User Data.sql

--- Joining Event and Pseudonymous User Data.sql
+++ Joining Event and Pseudonymous User Data.sql
@@ -1,4 +1,4 @@
 SELECT *, DATETIME(timestamp_micros(event_timestamp), 'Europe/Paris') AS event_datetime
-FROM `the-.analytics_409026738.events_*` AS events
-INNER JOIN `the-.analytics_409026738.pseudonymous_users_*` AS users
+FROM `the-.analytics_.events_*` AS events
+INNER JOIN `the-.analytics_.pseudonymous_users_*` AS users
 ON users.pseudo_user_id = events.user_pseudo_id;
